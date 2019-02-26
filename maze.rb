1b0278777057fc080cefb7ab4e0b5bc0c908980f
Holden Hinkle <holdenhinkle@gmail.com>
2019-02-26 10:35:29 -0500

    app creates all board permutations with start and finish sqyares

--- maze.rb
+++ maze.rb
@@ -1,10 +1,13 @@
+require 'pry'
+
 # A hash of all boards, with board_type (x/y dimensions) hashes that contain the 
 # boards = { board_5x2: [board_object, board_object], board_5x3: [board_object] }
-class AllBoards
-  attr_accessor :boards
+class Boards
+  attr_reader :boards
 
   def initialize(max)
-    @boards = create_boards(3, 2, max)
+    @boards = {}
+    create_boards(3, 2, max)
   end
 
   private
@@ -12,76 +15,88 @@ class AllBoards
   def create_boards(x_axis, y_axis, max)
     y_axis.upto(max) do |y_length|
       x_axis.upto(max) do |x_length|
-        boards["board#{x_length}x#{y_length}".to_sym] = Board.new(x_length, y_length)
+        boards["board_#{x_length}x#{y_length}".to_sym] = Board.new(x_length, y_length)
       end
     end
   end
 end
 
-# Each board is comprised of a grid that has a status (valid/invalid) and one or more solutions (if valid)
-# This is a board, which contains the grid, number of barrier squares, board_status 
-# (valid, invalid), solutions
-# board = { grid: [square_object, square_object..n], status: :valid, solutions: [] }
+# Each board contains
+# size
+# x-axis value
+# y-axis value
+# an array of grids for the board size
 class Board
-  attr_reader :grid
-  attr_accessor :status, :solutions
+  attr_reader :size, :x_axis_value, :y_axis_value, :grids
 
   def initialize(x, y)
-    @grid = create_grid(x, y)
-    @status = nil
-    @solutions = []
-  end
-
-  private
-
-  def create_grid(x, y)
-    #  (barrier_range(x, y).min).upto(barrier_range(x, y).max) do |num_barriers|
-      
-    # given grid layout, like 5 x 3
-    # we need to do three iterations:
-    # where start is
-    # where finish is
-    # number of barriers
-    # where barrier(s) are
-
-    return is a square
-
-  end
-
-  def barrier_range(x, y)
-    grid_size = [x, y].sort
-    case grid_size
-    when [2, 3] then [1]
-    when [3, 3] then [1, 2]
-    when [3, 4] then [1, 2]
-    when [4, 4] then [2, 3]
-    when [4, 5] then [2, 5]
-    when [5, 5] then [3, 5]
-    end
-  end
-end
-  
-# Each grid contains and array of sqaures, x and y values, and number of barriers in the grid
-# grid = [ [square_object, square_object, square_object],
-#          [square_object, square_object, square_object] ]
-class Grid
-  attr_reader :x_cord, :y_cord
-
-  def initialize(x, y)
+    @size = x * y
     @x_axis_value = x
     @y_axis_value = y
-    @number_of_barrier_squares = number_of_barrier_squares
-    @squares = draw_grid(x, y, number_of_barrier_squares)
+    @grids = create_grids
   end
 
   private
 
-  def draw_grid(x, y, number_of_barrier_squares)
+  def create_grids
+    # start and finish don't loop around - fix this
+    grids = []
+    0.upto(size - 1) do |start|
+      (start + 1).upto(size - 1) do |finish|
+        grids << Grid.new(size, start, finish)
+      end
+    end
+    grids << Grid.new(size, 1, 0)
+  end
+
+  # def barrier_range(x, y)
+  #   grid_size = [x, y].sort
+  #   case grid_size
+  #   when [2, 3] then [1]
+  #   when [3, 3] then [1, 2]
+  #   when [3, 4] then [1, 2]
+  #   when [4, 4] then [2, 3]
+  #   when [4, 5] then [2, 5]
+  #   when [5, 5] then [3, 5]
+  #   end
+  # end
+end
+  
+# Each grid contains:
+# array of sqaures
+# grid status
+# grid solutions
+class Grid
+  attr_reader :squares
+  attr_accessor :status, :solutions
+
+  def initialize(size, start_index, finish_index)
+    @squares = create_grid(size, start_index, finish_index)
+    @status = nil
+    @solutions = []
+    # @number_of_barrier_squares = number_of_barrier_squares
+  end
+
+  private
+
+  def create_grid(size, start_index, finish_index)
+    grid = []
+    size.times do |index|
+      grid << if index == start_index
+                Square.new(:taken, :start)
+              elsif index == finish_index
+                Square.new(:not_taken, :finish)
+              else
+                Square.new(:not_taken, :normal)
+              end
+    end
+    grid
   end
 end
 
 # Each square has a status and a type
 class Square
+  attr_reader :type
   attr_accessor :status
 
   def initialize(status, type)
@@ -90,7 +105,7 @@ class Square
   end
 end
 
-boards = AllBoards.new(5) # 5 is the max x/y grid coordinate value, which will create all grids
-                          # up to that size.
-
-
+max_board_dimension = 3
+boards = Boards.new(max_board_dimension)
+binding.pry
+puts boards
