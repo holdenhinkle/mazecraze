2966a33ef68bc6d20b854c5a182e6e816a20b2d4
Holden Hinkle <holdenhinkle@gmail.com>
2019-03-02 14:14:07 -0500

    refactor #solve and #attempt because the block in solve wasn't returing to the mothod from which it was called due to recursion so a number of grids weren't being solved

--- maze.rb
+++ maze.rb
@@ -35,37 +35,17 @@ class Board
 
   private
 
-  # for testing:
   def create_grids(board)
     counter = starting_file_number(board[:level]) + 1
-    test_lines = [#["s1", "b", "f1", "n", "n", "n"], 
-                  ["n", "n", "n", "f1", "b", "s1"], 
-                  ["f1", "b", "s1", "n", "n", "n"]]
-                  #["n", "n", "n", "s1", "b", "f1"]]
-    test_lines.each do |line|
-      grid = Grid.new(board, line)
-      puts line
-      puts "Valid finish square?: #{grid.valid_finish_squares?}"
-      puts "One solution? #{grid.one_solution?}"
+    file_path = permutations(layout)
+    File.open(file_path, "r").each_line do |grid_layout|
+      grid = Grid.new(board, JSON.parse(grid_layout))
       next unless grid.valid?
       save_grid!(grid, counter)
       counter += 1
     end
-    # FileUtils.rm(file_path)
+    FileUtils.rm(file_path)
   end
-
-  # working version:
-  # def create_grids(board)
-  #   counter = starting_file_number(board[:level]) + 1
-  #   file_path = permutations(layout)
-  #   File.open(file_path, "r").each_line do |grid_layout|
-  #     grid = Grid.new(board, JSON.parse(grid_layout))
-  #     next unless grid.valid?
-  #     save_grid!(grid, counter)
-  #     counter += 1
-  #   end
-  #   FileUtils.rm(file_path)
-  # end
 
   def starting_file_number(level)
     largest_number = 0
@@ -156,7 +136,7 @@ class Grid
     @y = board[:y]
     @level = board[:level]
     @solutions = []
-    solve({ path: [start_square_index], grid: self })
+    solve([{ path: [start_square_index], grid: self }])
   end
 
   def valid?
@@ -235,9 +215,9 @@ class Square
   end
 end
 
-boards = [{ x: 3, y: 2, num_starts: 1, num_barriers: 2, level: 1 }]
+boards = [{ x: 3, y: 2, num_starts: 1, num_barriers: 1, level: 1 }]
 
-# boards = [{ x: 3, y: 3, num_starts: 1, num_barriers: 1, level: 1 },
+# boards = [{ x: 3, y: 2, num_starts: 1, num_barriers: 1, level: 1 },
 #           { x: 3, y: 3, num_starts: 1, num_barriers: 2, level: 1 }]
 
 # boards = [{ x: 3, y: 2, num_starts: 1, num_barriers: 1, level: 1 },
--- solvable.rb
+++ solvable.rb
@@ -3,32 +3,36 @@ module Solvable
     solutions.size == 1
   end
 
-  def attempt(current_attempt)
+  def attempt(current_attempt, process_attempt)
     current_square = current_attempt[:path].last
-    yield(current_attempt, square_index_above(current_square)) if
+    process_attempt.call(current_attempt, square_index_above(current_square)) if
       not_taken_square_above?(current_square, current_attempt[:grid])
-    yield(current_attempt, square_index_right(current_square)) if
+    process_attempt.call(current_attempt, square_index_right(current_square)) if
       not_taken_square_right?(current_square, current_attempt[:grid])
-    yield(current_attempt, square_index_below(current_square)) if
+    process_attempt.call(current_attempt, square_index_below(current_square)) if
       not_taken_square_below?(current_square, current_attempt[:grid])
-    yield(current_attempt, square_index_left(current_square)) if
+    process_attempt.call(current_attempt, square_index_left(current_square)) if
       not_taken_square_left?(current_square, current_attempt[:grid])
   end
 
   def solve(new_attempt)
-    attempt(Marshal.load(Marshal.dump(new_attempt))) do |current_attempt, next_square|
+    new_attempts = new_attempt
+    process_attempt = Proc.new do |current_attempt, next_square|
       current_attempt = Marshal.load(Marshal.dump(current_attempt))
       current_grid = current_attempt[:grid]
-      current_path = current_attempt[:path]
-      current_path << next_square
+      current_path = current_attempt[:path].push(next_square)
       current_grid.squares[next_square].taken!
+
       if current_grid.squares[next_square].finish_square? &&
          current_grid.all_squares_taken?
         solutions << current_path
       elsif current_grid.squares[next_square].normal_square?
-        solve({ path: current_path, grid: current_grid })
+        new_attempts << ({ path: current_path, grid: current_grid })
       end
-      break
+    end
+
+    until new_attempts.empty?
+      attempt(Marshal.load(Marshal.dump(new_attempts.shift)), process_attempt)
     end
   end
 end
