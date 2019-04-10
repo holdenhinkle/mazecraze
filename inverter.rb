ad6847089d946d5f01f9d24ba61f31e1d3c1c67b
Holden Hinkle <holdenhinkle@gmail.com>
2019-04-10 13:05:11 -0400

    write Inverter class

--- inverter.rb
+++ inverter.rb
@@ -8,15 +8,29 @@ class Inverter
 
   def all_inversions(maze)
     { original: maze, 
-      horizontal: horizontal(maze),
-      vertical: vertical(maze) }
+      horizontal: horizontal(maze.clone),
+      vertical: vertical(maze.clone) }
   end
 
-  def horizontal(maze)
+  private
 
+  # if any of the following methods are made public, the maze local
+  # variable must be cloned.
+
+  def horizontal(maze)
+    process_inversion(maze) { |maze| maze.shift(x).reverse! }
   end
 
   def vertical(maze)
+    process_inversion(maze) { |maze| maze.pop(x) }
+  end
 
+  def process_inversion(maze)
+    flipped_maze = []
+    y.times do
+      flipped_maze << yield(maze)
+    end
+    flipped_maze.flatten!
+    flipped_maze
   end
 end
