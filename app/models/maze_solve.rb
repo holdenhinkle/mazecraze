231ab5bfc0316bc2d45edbe161b6eeb78c1b1b09
Holden Hinkle <holdenhinkle@gmail.com>
2019-05-30 11:50:49 -0400

    refactor MazeSolve module

--- app/models/maze_solve.rb
+++ app/models/maze_solve.rb
@@ -1,48 +1,40 @@
 module MazeSolve
-  def one_solution?
-    solutions.size == 1
-    # true
-  end
-
-  def solve(new_attempt)
-    new_attempts = new_attempt
-
-    process_attempt = Proc.new do |current_attempt, next_square|
-      current_attempt = Marshal.load(Marshal.dump(current_attempt))
-      current_maze = current_attempt[:maze]
-      current_path = current_attempt[:path].push(next_square)
-      mark_square_taken(current_maze, current_path, current_maze.squares[next_square])
-      outcome = check_attempt(current_maze, current_path, next_square)
-      solutions << outcome if outcome.is_a? Array # solved
-      new_attempts << outcome if outcome.is_a? Hash # continue
-    end
-
-    until new_attempts.empty?
-      attempt(new_attempts.shift, process_attempt)
+  def solve(attempts)
+    until attempts.empty?
+      attempt(attempts.shift) do |current_attempt, next_square|
+        current_attempt = Marshal.load(Marshal.dump(current_attempt))
+        current_maze = current_attempt[:maze]
+        current_path = current_attempt[:path].push(next_square)
+        mark_square_taken!(current_maze, current_path, current_maze.squares[next_square])
+        outcome = check_attempt(current_maze, current_path, next_square)
+        next if outcome.nil?
+        solutions << outcome[:solution] && next if outcome[:solution]
+        attempts << outcome
+      end
     end
   end
 
-  def attempt(current_attempt, process_attempt)
+  def attempt(current_attempt)
     current_square = current_attempt[:path].last
     current_maze = current_attempt[:maze]
-    process_attempt.call(current_attempt, square_index_above(current_square)) if
+    yield(current_attempt, square_index_above(current_square)) if
       valid_move?('above', current_square, current_maze)
-    process_attempt.call(current_attempt, square_index_right(current_square)) if
+    yield(current_attempt, square_index_right(current_square)) if
       valid_move?('right', current_square, current_maze)
-    process_attempt.call(current_attempt, square_index_below(current_square)) if
+    yield(current_attempt, square_index_below(current_square)) if
       valid_move?('below', current_square, current_maze)
-    process_attempt.call(current_attempt, square_index_left(current_square)) if
+    yield(current_attempt, square_index_left(current_square)) if
       valid_move?('left', current_square, current_maze)
   end
 
-  def mark_square_taken(_, _, square)
+  def mark_square_taken!(_, _, square)
     square.taken!
   end
 
   def check_attempt(current_maze, current_path, next_square)
     if current_maze.squares[next_square].finish_square? &&
        current_maze.all_squares_taken?
-      current_path
+      { solution: current_path }
     elsif current_maze.squares[next_square].normal_square?
       { path: current_path, maze: current_maze }
     end
@@ -53,18 +45,27 @@ module SolveBridgeMaze
   def check_attempt(current_maze, current_path, next_square)
     if current_maze.squares[next_square].finish_square? &&
        current_maze.all_squares_taken?
-      current_path
+      { solution: current_path }
     elsif current_maze.squares[next_square].normal_square? ||
           current_maze.squares[next_square].bridge_square?
       { path: current_path, maze: current_maze }
     end
   end
 
-  def mark_square_taken(_, current_path, square)
-    square.type != :bridge ? square.taken! : update_bridge_square(current_path, square)
+  def mark_square_taken!(_, current_path, square)
+    if square.type == :bridge
+      update_bridge_square!(current_path, square)
+    else
+      square.taken!
+    end
+    # if square.type != :bridge
+    #   square.taken!
+    # else
+    #   update_bridge_square!(current_path, square)
+    # end
   end
 
-  def update_bridge_square(current_path, square)
+  def update_bridge_square!(current_path, square)
     current_square_index = current_path.last
     previous_square_index = current_path[current_path.size - 2]
     if square_index_above(current_square_index) == previous_square_index ||
@@ -78,7 +79,7 @@ module SolveBridgeMaze
 end
 
 module SolveTunnelMaze
-  def mark_square_taken(current_maze, current_path, square)
+  def mark_square_taken!(current_maze, current_path, square)
     square.taken!
     return unless square.tunnel_square?
     other_end_of_tunnel_square =
@@ -88,10 +89,10 @@ module SolveTunnelMaze
   end
 
   def check_attempt(current_maze, current_path, next_square)
-    if current_maze.squares[next_square].finish_square? && # if solved
+    if current_maze.squares[next_square].finish_square? &&
        current_maze.all_squares_taken?
-      current_path
-    elsif current_maze.squares[next_square].normal_square? || # continue
+      { solution: current_path }
+    elsif current_maze.squares[next_square].normal_square? ||
           current_maze.squares[next_square].tunnel_square?
       { path: current_path, maze: current_maze }
     end
@@ -99,7 +100,7 @@ module SolveTunnelMaze
 end
 
 module SolvePortalMaze
-  def mark_square_taken(current_maze, current_path, square)
+  def mark_square_taken!(current_maze, current_path, square)
     square.taken!
     return unless square.portal_square?
     other_end_of_portal_square = if top_border_indices.include?(square.index) ||
@@ -115,10 +116,10 @@ module SolvePortalMaze
   end
 
   def check_attempt(current_maze, current_path, next_square)
-    if current_maze.squares[next_square].finish_square? && # if solved
+    if current_maze.squares[next_square].finish_square? &&
        current_maze.all_squares_taken?
-      current_path
-    elsif current_maze.squares[next_square].normal_square? || # continue
+      { solution: current_path }
+    elsif current_maze.squares[next_square].normal_square? ||
           current_maze.squares[next_square].portal_square?
       { path: current_path, maze: current_maze }
     end
