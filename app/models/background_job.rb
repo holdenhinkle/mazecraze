8d1cd12b67276e147424bfc513538cab14e680d9
Holden Hinkle <holdenhinkle@gmail.com>
2019-05-31 13:50:47 -0400

    write BackgroundJob#all_jobs

--- app/models/background_job.rb
+++ app/models/background_job.rb
@@ -3,6 +3,8 @@ class BackgroundJob
                  generate_maze_permutations
                  generate_maze_candidates).freeze
 
+  JOB_STATUSES = %w(queued processing completed failed).freeze
+
   attr_reader :job_type, :params
 
   def initialize(job)
@@ -15,6 +17,22 @@ class BackgroundJob
     results = db.query(sql, *params)
     db.disconnect
     results
+  end
+
+  def query(sql, *params)
+    db = DatabaseConnection.new
+    results = db.query(sql, *params)
+    db.disconnect
+    results
+  end
+
+  def self.all_jobs
+    JOB_STATUSES.each_with_object({}) do |status, jobs|
+      sql = "SELECT * FROM background_jobs WHERE status = $1 ORDER BY updated DESC LIMIT 10;"
+      jobs[status] = query(sql, status)
+      sql = "SELECT COUNT(id) FROM background_jobs WHERE status = $1;"
+      jobs[status + '_count'] = query(sql, status)
+    end
   end
 
   def work
