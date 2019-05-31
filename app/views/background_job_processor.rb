f4865e3414f00f2e005d88f4480f66b309bf6a61
Holden Hinkle <holdenhinkle@gmail.com>
2019-05-31 16:56:30 -0400

    create BackgroundJobProcessor class

---
+++ app/views/background_job_processor.rb
@@ -0,0 +1,26 @@
+class BackgroundJobProcessor
+  def self.query(sql, *params)
+    db = DatabaseConnection.new
+    results = db.query(sql, *params)
+    db.disconnect
+    results
+  end
+
+  def run
+    job = next_queued_job
+  end
+
+  private
+
+  def query(sql, *params)
+    db = DatabaseConnection.new
+    results = db.query(sql, *params)
+    db.disconnect
+    results
+  end
+
+  def next_queued_job
+    sql = "SELECT id, job_type, params FROM background_jobs ORDER BY created LIMIT 1;"
+    query(sql)
+  end
+end
