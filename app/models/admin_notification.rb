1afc0f852d563f5350226305d2502ca3533b6c36
Holden Hinkle <holdenhinkle@gmail.com>
2019-06-03 12:16:48 -0400

    write AdminNotification class

---
+++ app/models/admin_notification.rb
@@ -0,0 +1,26 @@
+class AdminNotification
+  attr_reader :notification
+
+  def initilaize(notification)
+    @notification = notification
+  end
+
+  def save!
+    sql = "INSERT INTO admin_notifications (notification) VALUES ($1);"
+    query(sql, notification)
+  end
+
+  def delivered!(id)
+    sql = "UPDATE table admin_notifications SET delivered = $1, updated = $2 WHERE id = $3;"
+    query(sql, TRUE, NOW, id)
+  end
+
+  private
+
+  def query(sql, *params)
+    db = DatabaseConnection.new
+    results = db.query(sql, *params)
+    db.disconnect
+    results
+  end
+end
