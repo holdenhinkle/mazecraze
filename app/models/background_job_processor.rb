e4a0ba52f1e1b802396cc56687029621043aa3d4
Holden Hinkle <holdenhinkle@gmail.com>
2019-06-04 13:33:11 -0400

    use Thread instead of Process. refactor class

--- app/models/background_job_processor.rb
+++ app/models/background_job_processor.rb
@@ -1,50 +1,57 @@
 class BackgroundJobProcessor
-  attr_accessor :ppid, :pid
+  attr_reader :jobs, :number_of_threads
+  attr_accessor :threads
 
-  def initialize
-    @ppid = nil
-    @pid = nil
+  def initialize(number_of_threads = 1)
+    @jobs = Queue.new
+    @number_of_threads = number_of_threads
+    @threads = []
   end
 
-  def self.query(sql, *params)
-    db = DatabaseConnection.new
-    results = db.query(sql, *params)
-    db.disconnect
-    results
+  def self.running?
+    Thread.list.each do |thread|
+      thread = thread.to_s
+      # return true if thread.include?('background_job_processor.rb') &&
+      #                thread.include?('run')
+      return true if thread.include?('background_job_processor.rb')
+    end
+    false
   end
 
-  def save!; end
+  def jobs?
+    !jobs.empty?
+  end
 
-  def run
-    if BackgroundJob.all_jobs_of_status_type('queued').values.any?
-    # while BackgroundJob.all_jobs_of_status_type('queued').values.any?
-      job = BackgroundJob.next_queued_job.first
-      self.pid = Process.fork do
-        puts "Backgroud job, pid #{Process.pid}, processing..."
-        self.ppid = Process.ppid
-        log_new_process
-        BackgroundJob.new({ type: job['job_type'], params: job['params'] }).run
-        puts "Background job, finished, exiting"
-      end
-      puts "Background process, pid #{Process.pid}, waiting on background Job, pid #{pid}"
-      # Process.wait(pid)
-      puts "Background process exiting"
-      # update job
-      # update process
+  def running?
+    !jobs.closed?
+  end
+
+  # def dequeue
+  #   jobs.pop(true)
+  # end
+
+  def wait_for_job
+    jobs.pop(false)
+  end
+
+  def enqueue
+    BackgroundJob.all_jobs_of_status_type('queued').each do |job|
+      jobs << { id: job['id'],
+                type: job['job_type'],
+                params: JSON.parse(job['params']) }
     end
   end
 
-  private
-
-  def query(sql, *params)
-    db = DatabaseConnection.new
-    results = db.query(sql, *params)
-    db.disconnect
-    results
-  end
-
-  def log_new_process
-    sql = "INSERT INTO background_processes (ppid) VALUES ($1);"
-    query(sql, ppid)
+  def run
+    number_of_threads.times do
+      threads << Thread.new do
+        while running? || jobs?
+          job = wait_for_job
+          BackgroundJob.new(job).run if job
+          # log thread
+          # add new jobs to queue
+        end
+      end
+    end
   end
 end
