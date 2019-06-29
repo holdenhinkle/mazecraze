9501326e12e914e62e4c6fa5ee7165e7b1c40a6f
Holden Hinkle <holdenhinkle@gmail.com>
2019-06-29 13:11:30 -0400

    write helper methods to format background job data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,25 +5,43 @@ module ApplicationHelper
     @title ? "#{@title} :: Maze Craze" : 'Maze Craze'
   end
 
-  def replace_underscores_with_space(str)
-    return str unless str.include?('_')
-    str.gsub!('_', ' ')
+  def underscores_to_spaces(string)
+    return string unless string.include?('_')
+    string.tr!('_', ' ')
   end
 
-  # REDO THIS
-  def array_to_string(array)
-    result = ""
-    array.each_with_index do |element, index|
-      return element if array.size == 1
-      return "#{element} and #{array[index + 1]}" if array.size == 2
-      result << element
-      if index == array.size - 2
-        return result << ", and #{array[index + 1]}"
-      else 
-        result << ', '
-      end
-    end
+  def capitalize_words(string)
+    string.split(' ').map(&:capitalize).join(' ')
   end
+
+  def format_background_job_params(params)
+    JSON.parse(params).each_with_object([]) do |(key, value), array|
+      string = ''
+      string << capitalize_words(underscores_to_spaces(+key)) + ': '
+      string << value.capitalize
+      array << string
+    end.join(', ')
+  end
+
+  # def replace_underscores_with_space(str)
+  #   return str unless str.include?('_')
+  #   str.gsub!('_', ' ')
+  # end
+
+  # REDO THIS
+  # def array_to_string(array)
+  #   result = ""
+  #   array.each_with_index do |element, index|
+  #     return element if array.size == 1
+  #     return "#{element} and #{array[index + 1]}" if array.size == 2
+  #     result << element
+  #     if index == array.size - 2
+  #       return result << ", and #{array[index + 1]}"
+  #     else 
+  #       result << ', '
+  #     end
+  #   end
+  # end
 
   def add_hashes_to_session_hash(hash)
     hash.each { |key, value| session[key] = value }
